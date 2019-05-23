617de07df2f9c35c295455c04a7edcf7e0b68106
AswinManohar <aswinbio@gmail.com>
2019-05-23 13:52:01 +0200

    Add files via upload

---
+++ bokeh_wrapper.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+filename=$1
+
+result=$(sqlite3 /hercules/u/aswin/database/Processed.db "select result_path from headers where file_name='$filename'")
+
+file_location=$(sqlite3 /hercules/u/aswin/database/Processed.db "select file_location from headers where file_name='$filename'")
+
+echo $result $file_location
+
+cd $result
+
+ln -s $file_location 
+
+
+
+echo "Creating filterbank file\n"
+filterbank $file_location -n 8 -o $filename".fil"
+
+tar -xvf *_singlepulse.tgz
+
+echo "tar done"
+
+touch spfiles_combined
+ls spfiles_combined
+
+echo "loop starting"
+for spfile in $(ls -1rt *.singlepulse)
+   do cat $spfile >> spfiles_combined
+done
+echo "loop end" 
+
+touch spfiles_combined.sorted
+
+echo "grep coming"
+grep -v "# DM" spfiles_combined | sort -gr -k 2 >> spfiles_combined.sorted
+
+ 
+bokeh  serve --show ~/test.py --args $filename".fil" $result/spfiles_combined.sorted
+
+echo "bokeh  serve --show ~/test.py --args $filename".fil" $result/spfiles_combined.sorted"
+exit
